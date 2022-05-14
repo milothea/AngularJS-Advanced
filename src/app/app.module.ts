c04e2990c8b04f61eb0593201ff933382125e249
Liubou Kavaleuskaya <liuboukavaleuskaya@IBA600308AA8C.local>
2022-05-14 14:28:17 +0300

    Task 2: create auth module * Create auth module * Create login and registration draft components * Add app-routing and auth-routing * Configure app-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthRoutingModule } from './auth/auth-routing.module';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistrationComponent } from './auth/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AuthRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
