9bf4c1605b23cd4451193e536a4c4c91eceaa2be
Liubou Kavaleuskaya <liuboukavaleuskaya@IBA600308AA8C.local>
2022-05-14 16:05:48 +0300

    Task 2: fix according to review * Replace direct import Login and Registration components to import AuthModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthRoutingModule } from './auth/auth-routing.module';
-import { LoginComponent } from './auth/login/login.component';
-import { RegistrationComponent } from './auth/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegistrationComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AuthModule,
     AuthRoutingModule
   ],
   providers: [],
