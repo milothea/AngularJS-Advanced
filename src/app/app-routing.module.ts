c570d4d6ae5c8ae5180f1213d0128e29ddd97a53
Liubou Kavaleuskaya <liuboukavaleuskaya@192.168.8.24>
2022-05-14 16:55:50 +0300

    Task 2: refactor app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { AuthRoutingModule } from './auth/auth-routing.module';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
+
 @NgModule({
-  imports: [AuthRoutingModule],
-  exports: [AuthRoutingModule]
+  imports: [AuthRoutingModule, RouterModule.forRoot(routes)],
+  exports: [AuthRoutingModule, RouterModule]
 })
 export class AppRoutingModule { }
