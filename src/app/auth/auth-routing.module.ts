2b334a767dd094e7512393aae1ed1904159e489f
Liubou Kavaleuskaya <liuboukavaleuskaya@IBA600308AA8C.local>
2022-05-19 22:24:21 +0300

    Task 2: remove duplicate route

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -4,7 +4,6 @@ import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'registration', component: RegistrationComponent }
 ];
